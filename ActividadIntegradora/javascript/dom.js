796a22d47b3672fffe59a8b90b49c7de6758bab7
constanza-diazalvarez <56646781+constanza-diazalvarez@users.noreply.github.com>
2024-03-12 21:13:27 -0300

    Delete ActividadIntegradora directory

--- ActividadIntegradora/javascript/dom.js
+++
@@ -1,9 +0,0 @@
-tag_p_elegido.innerHTML = "primer texto";
-tag_p_elegido2_sq.innerHTML = "segundo texto";
-tag_p_elegido2_sq.className = "p-rojo";
-
-const tag_p_nuevo = document.createElement('p');
-tag_p_nuevo.innerHTML = "este p fue agregado con js";
-tag_p_nuevo.className = "p-rojo";
-
-tag_
